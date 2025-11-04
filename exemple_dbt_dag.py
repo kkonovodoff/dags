9ef3ec8d4aca29357adbe6359630c0ccdf0d09fb
kkonovod <kostas@enix.fr>
2025-11-04 18:38:43 +0100

    wip: update bash commande from dbt dag

--- exemple_dbt_dag.py
+++ exemple_dbt_dag.py
@@ -17,6 +17,6 @@ dag = DAG('dbt_dag', default_args=default_args, schedule_interval=timedelta(days
 
 run_dbt_debug = BashOperator(
     task_id='run_dbt_debug',
-    bash_command='ls -lah',
+    bash_command='ls -lah /opt/aiflow/dbt/digipoc ; dbt debug --project-dir /opt/aiflow/dbt/digipoc -t prod',
     dag=dag
 )
