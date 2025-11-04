d059fa8c1c7ed2dfd7ec76b6f41f9a59c9f5f7db
kkonovod <kostas@enix.fr>
2025-11-04 16:00:06 +0100

    feat: add new dag using KubernetesPodOperator·

--- exemple_dbt_dag.py
+++ exemple_dbt_dag.py
@@ -2,6 +2,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -9,9 +10,11 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+
 # Create the DAG with the specified schedule interval
 dag = DAG('dbt_dag', default_args=default_args, schedule_interval=timedelta(days=1))
 # Define the dbt run command as a BashOperator
+
 run_dbt_model = BashOperator(
     task_id='run_dbt_model',
     bash_command='dbt run',
