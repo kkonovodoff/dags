c1004e2360ef7b0bae9f986a3f6e06c876150870
kkonovod <kostas@enix.fr>
2025-11-04 18:43:54 +0100

    wip: fix typo in path to dbt project

--- exemple_dbt_dag.py
+++ exemple_dbt_dag.py
@@ -17,6 +17,6 @@ dag = DAG('dbt_dag', default_args=default_args, schedule_interval=timedelta(days
 
 run_dbt_debug = BashOperator(
     task_id='run_dbt_debug',
-    bash_command='ls -lah /opt/aiflow/dags/repo/dbt/digipoc ; dbt debug --project-dir /opt/aiflow/dags/repo/dbt/digipoc -t prod',
+    bash_command='ls -lah /opt/airflow/dags/repo/dbt/digipoc ; dbt debug --project-dir /opt/airflow/dags/repo/dbt/digipoc -t prod',
     dag=dag
 )
